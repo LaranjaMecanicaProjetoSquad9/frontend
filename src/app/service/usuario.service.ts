721b4ef9f1f70f80c20ce58e928a093fbed6a5e7
Isaías Roberto <isaias.reng@gmail.com>
2022-04-13 09:11:36 -0300

    primeira tentativa de comunicação com o backend  usando o cadastro

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -24,19 +24,19 @@ export class UsuarioService {
   }
 
   getAllUser(): Observable<Usuario[]>{
-    return this.http.get<Usuario[]>('https://ecotendabe.herokuapp.com/usuarios/all', this.token)
+    return this.http.get<Usuario[]>('https://technicalshare-api.herokuapp.com/usuario/all', this.token)
   }
 
   getIdUser(id: number): Observable<Usuario>{
-    return this.http.get<Usuario>(`https://ecotendabe.herokuapp.com/usuarios/${id}`, this.token)
+    return this.http.get<Usuario>(`https://technicalshare-api.herokuapp.com/usuario/${id}`, this.token)
   }
 
   putUser(usuario: Usuario): Observable<Usuario>{
-    return this.http.put<Usuario>('https://ecotendabe.herokuapp.com/usuarios/alterar', usuario, this.token)
+    return this.http.put<Usuario>('https://technicalshare-api.herokuapp.com/usuario/alterar/', usuario, this.token)
   }
 
   deleteUser(id: number){
-    return this.http.delete<Usuario>(`https://ecotendabe.herokuapp.com/usuarios/${id}`, this.token)
+    return this.http.delete<Usuario>(`https://technicalshare-api.herokuapp.com/usuario${id}`, this.token)
   }
 
 }
